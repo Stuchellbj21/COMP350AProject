3da010886696763a027362479ea970a7552cc0f1
ANASTASIBM18 <AnastasiBM18@gcc.edu>
2024-03-19 14:11:25 -0400

    Fixed UI for Main and made some tweaks to schedule - Ben

--- src/main/java/Schedule.java
+++ src/main/java/Schedule.java
@@ -252,4 +252,28 @@ public class Schedule {
         courses.clear();
         return true;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
