6c6e0621e7d177bfae552f3cc81c28d2d3d52328
VolticuS6 <100294967+Stuchellbj21@users.noreply.github.com>
2024-03-27 14:49:11 -0400

    AccountTest actually works

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,24 +1,20 @@
 import jdk.jshell.spi.ExecutionControlProvider;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.EmptyStackException;
-import java.util.List;
 
 class AccountTest {
-
-    Schedule currentSchedule;
     String sched_name;
-    List<String> schednames;
     // Create Account
     Account myAccount = new Account("Stuchellbj21", "53444", Major.COMP );
     @Test
     public void  ConstructorTest() throws Exception{
         // Test gets for Account variables
-        if(myAccount.getUsername().equals("Stuchellbj21")) { }else{throw new Exception("failed name get");}
-        // if(myAccount.verify_password(53444)) { throw new Exception("failed passwordhash get");}
+        if(!myAccount.getUsername().equals("Stuchellbj21")){throw new Exception("failed name get");}
+        if(!myAccount.verify_password("53444")) { throw new Exception("failed passwordhash get");}
         if(myAccount.getMajor()!= Major.COMP) { throw new Exception("failed major get");}
     }
 
@@ -27,50 +23,44 @@ class AccountTest {
         Account newAcc = new Account();
         newAcc.setUsername("MyNewUsername");
         newAcc.setMajor(Major.ACCT);
-        if(newAcc.getUsername().equals("MyNewUsername")){ } else {throw new Exception("failed username get"); }
-        if(newAcc.getMajor() != Major.ACCT){ throw new Exception("failed major set");}
+        if(!newAcc.getUsername().equals("MyNewUsername")){throw new Exception("failed username get");}
+        if(newAcc.getMajor() != Major.ACCT){throw new Exception("failed major set");}
     }
-
     @Test
-    void changeMajor() throws Exception {
+    void changeMajor() throws Exception{
         myAccount.change_major(Major.COMP);
-        if (myAccount.getMajor() != Major.COMP) {
-            throw new Exception("failed major change");
-        }
+        if (myAccount.getMajor() != Major.COMP){throw new Exception("failed major change");}
     }
     @Test
-    void change_username() throws Exception {
+    void change_username() throws Exception{
         myAccount.change_username("Bstuchell15");
-        if (myAccount.getUsername().equals("Bstuchell15")) {
-            throw new Exception("failed username change");
-        }
+        if (myAccount.getUsername().equals("Bstuchell15")){throw new Exception("Failed username change");}
     }
     @Test
-    void testSaveSchedule() throws Exception {
-        currentSchedule.save_schedule(sched_name);
-
+    void testSaveSchedule() throws Exception{
+        myAccount.save_schedule(sched_name);
+        if(!myAccount.has_schedule(sched_name)){throw new Exception("Schedule not found");}
     }
     @Test
     void testLoadSchedule() throws Exception {
-        schednames.load_schedule(sched_name);
+        myAccount.load_schedule(sched_name);
     }
 
     @Test
     void testDeleteSchedule() throws Exception{
-        delete_schedule(sched_name);
+        myAccount.delete_schedule(sched_name);
+        if(myAccount.has_schedule(sched_name)){throw new Exception("Schedule still exists");}
     }
 
     @Test
     void testDeleteAccount() throws Exception{
-        schednames.delete_account(myAccount);
-        if(myAccount != null){
-            throw new Exception("myAccount still exists");
-        }
+        //AccountList.delete_account(myAccount);
+        if(myAccount != null){throw new Exception("myAccount still exists");}
     }
 
     @Test
     void testHasSchedule() throws Exception {
-        schednames.save_schedule(myAccount);
-        if(schednames.has_schedule("myAccount")){ } else { throw new Exception("Schedule not found"); }
+        myAccount.save_schedule(sched_name);
+        if(!myAccount.has_schedule("sched_name")){throw new Exception("Schedule not found");}
     }
 }
