5522a19ae5f80a340e61246358122cb506376e43
KUHNSNJ21 <n8kuhns@gmail.com>
2024-04-05 01:19:44 -0400

    toString + other changes to filters + filters now work with search

--- src/main/java/DaysFilter.java
+++ src/main/java/DaysFilter.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -5,9 +6,14 @@ public class DaysFilter extends Filter {
     private Set<Character> days;
 
     public DaysFilter(List<Course> courses, Set<Character> days) {
-        super.filteron = FilterType.DAYS;
+        super(FilterType.DAYS);
         this.days = days;
         apply_to(courses); // A filter is applied automatically when it is created
+    }
+
+    public DaysFilter() {
+        super(FilterType.DAYS);
+        days = null;
     }
 
     @Override
@@ -20,16 +26,27 @@ public class DaysFilter extends Filter {
 //                }
 //        }
 
-        // first loop runs backward to avoid concurrent modification exception
-        for (int i = courses.size()-1; i >= 0; i--) {
-            for (int j = 0; j < courses.get(i).getTimes().size(); j++) {
-                // if no day belonging to a course is found in the filter's set of days...
-                if(!days.contains(courses.get(i).getTimes().get(j).get_day())) {
-                    courses.remove(courses.get(i)); // get rid of it
+        // add removed to remove list and remove all at once to avoid concurrent modification error
+        ArrayList<Course> toremove = new ArrayList<>();
+        for (Course c : courses) {
+            if(c.getTimes() == null || c.getTimes().isEmpty()) {
+                toremove.add(c);
+                continue;
+            }
+            for (DayTime dt : c.getTimes()) {
+                // if the number of days in the filter is different from the number of days in the course
+                // or the course's day isn't in the filter -> remove the course
+                if(c.getTimes().size() != days.size() || !days.contains(dt.get_day())) {
+                    toremove.add(c); // get rid of it
+                    break;
                 }
             }
         }
+        courses.removeAll(toremove);
     }
+
+    @Override
+    public String toString() {return filteron.name() + " filter: " + days;}
 }
 //here we do for each DayTime in Course.times is the DayTime’s day in days and is
 //days.size() == times.size() true Todo: remove this comment
