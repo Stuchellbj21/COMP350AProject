d660438f45814d8f09afdd64141f4b263935bb05
KUHNSNJ21 <n8kuhns@gmail.com>
2024-05-02 12:35:26 -0400

    afl + overloaded search once again + made search work with courses already taken + added reset for when we leave an account

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -81,8 +81,12 @@ public class Search {
             }
             //add all courses up to threshold to results
             for(Course c : weighttocourse.get(i)) {
-                searchresults.add(c);
-                if(++n >= threshold) break;
+                //only add the course to search results if the course wasn't already taken by
+                //the current user
+                if(!Main.currentaccnt.already_took(c)) {
+                    searchresults.add(c);
+                    if (++n >= threshold) break;
+                }
             }
         }
         //filter results and return
@@ -100,6 +104,8 @@ public class Search {
     }
 
     public List<Course> search(String ss) {return search(ss,Integer.MAX_VALUE,true);}
+
+    public List<Course> search(String ss,int threshold) {return search(ss,threshold,true);}
 
     public List<Course> search(String ss,boolean sorted) {return search(ss,Integer.MAX_VALUE,sorted);}
 
@@ -198,6 +204,11 @@ public class Search {
         return filteredresults;
     }
 
+    public void reset() {
+        activefilters.clear();
+        search("",0);
+    }
+
     public List<Course> get_filtered_results() {return filteredresults;}
 
     public List<Filter> get_active_filters() {return activefilters;}
@@ -222,12 +233,12 @@ public class Search {
                 sorted = GeneralUtils.want_more('s');
                 break;
             } catch (NumberFormatException nfe) {
-                Main.autoflush.println("Error: " + in + " is not a valid integer. Enter an integer value.");
+                Main.afl.println("Error: " + in + " is not a valid integer. Enter an integer value.");
             } catch (IllegalArgumentException iae) {
-                Main.autoflush.println(iae.getMessage());
+                Main.afl.println(iae.getMessage());
             }
         }
         Main.search.search(GeneralUtils.input("Enter search string: "), sorted);
-        Main.autoflush.println(Main.search.to_str(threshold));
+        Main.afl.println(Main.search.to_str(threshold));
     }
 }
